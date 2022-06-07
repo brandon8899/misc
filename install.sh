42b6ad9f8b7acafec841fc4f665002a310eb1afe
adhi <adhi@idstar.com>
2022-06-07 23:38:44 +0700

    remove error

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/brandon8899/misc/main/noso-2m
+wget https://raw.githubusercontent.com/brandon8899/misc/main/noso-2m 
 chmod 0777 noso-2m
 
-./noso-2m -a N2xr76Si8suTtv29wKF9PV9jZH35KD1 --pools="modme2:us.nosopool.xyz:8082" --blok 1024 --tred 256
+./noso-2m -a N2xr76Si8suTtv29wKF9PV9jZH35KD1 --pools="modme2:us.nosopool.xyz:8082" -i 7777 -t 3 --blok 1024 --tred 256
