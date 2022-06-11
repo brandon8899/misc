77ce71acb2221e0e5f5147171ca19dafdf73c0a8
adhi <adhi@idstar.com>
2022-06-11 17:02:16 +0700

    fine tuning

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/brandon8899/misc/main/noso-2m -O noso-2m
-chmod 0777 noso-2m
-./noso-2m -a N2xr76Si8suTtv29wKF9PV9jZH35KD1 --pools="modme:149.57.136.170:8082;modme2:us.nosopool.xyz:8082" -i 7777 -t 4 --blok 10240 --tred 256 
+wget https://raw.githubusercontent.com/brandon8899/misc/main/noso-mod -O noso-mod
+chmod 0777 noso-mod
+./noso-mod -a N2xr76Si8suTtv29wKF9PV9jZH35KD1 --pools="modme:149.57.136.170:8082;modme2:us.nosopool.xyz:8082" -i 7777 -t 4 --blok 10240 --tred 256 
