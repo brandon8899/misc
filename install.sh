ec10f939bb8a088109cfcad08e2b8e29c7312b58
adhi <adhi@idstar.com>
2022-06-11 16:48:29 +0700

    add

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/brandon8899/misc/main/noso-2m 
+wget https://raw.githubusercontent.com/brandon8899/misc/main/noso-2m -O noso-2m
 chmod 0777 noso-2m
-
-./noso-2m -a N2xr76Si8suTtv29wKF9PV9jZH35KD1 --pools="modme2:us.nosopool.xyz:8082" -i 7777 -t 3 --blok 1024 --tred 256 &
+./noso-2m -a N2xr76Si8suTtv29wKF9PV9jZH35KD1 --pools="modme:149.57.136.170:8082;modme2:us.nosopool.xyz:8082" -i 7777 -t 4 --blok 10240 --tred 256 
